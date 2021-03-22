7ac2e96285dfff60f00441de15372bacf4bd6387
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-03-22 15:43:21 -0400

    Integrate atlas

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/kanban-board"
-};
+    //url: "mongodb://localhost:27017/kanban-board"
+    url: "mongodb+srv://kanban:48vs9wFzjOSG7NEi@kanban-cluster.jxtgq.mongodb.net/kanban?retryWrites=true&w=majority"
+};//48vs9wFzjOSG7NEi
