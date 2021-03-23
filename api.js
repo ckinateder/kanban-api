53a4a5b6a1d5f0afd0f1b42d29e160640955ff45
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-03-22 23:23:24 -0400

    Update CORS again

--- api.js
+++ api.js
@@ -32,7 +32,7 @@ app.use(express.urlencoded({ extended: true }));
 
 require("./app/routes/task.routes")(app);
 // set port, listen for requests
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
